8de62bec14aa730f606a425d1160a5e9f00d1a8a
Ryan T Kelly <Ryan.T.Kelly@chicagotrading.com>
2022-07-13 15:15:13 -0500

    Able to deploy to Ganache after fixing two small problems.

--- projects/Lottery/truffle-config.js
+++ projects/Lottery/truffle-config.js
@@ -44,7 +44,7 @@ module.exports = {
 
     ganache: {
       host: "127.0.0.1",     // Localhost (default: none)
-      port: 7545,            // Standard Ethereum port (default: none)
+      port: 5545,            // Standard Ethereum port (default: none)
       gas: 5000000,
       network_id: "*",       // Any network (default: none)
     },
